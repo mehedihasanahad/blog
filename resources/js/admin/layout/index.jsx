1508fd8c7f54f3e209cc136d5a46a9d61c84e7ad
Mehedi Hasan Ahad <mehedihasanahad07@gmail.com>
2024-08-31 23:43:51 +0600

    admin layout added

--- resources/js/admin/layout/index.jsx
+++ resources/js/admin/layout/index.jsx
@@ -1,9 +1,40 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer.jsx";
+import Header from './Header.jsx';
+import Sidebar from "./Sidebar.jsx";
+
+function toggleSidebar() {
+    const fixedSidebar = document.querySelector('.fixedSidebar');
+    const sidebar = document.querySelector('.sidebar');
+    const humburger = document.querySelector('.humburger');
+
+    // fake sidebar handling
+    sidebar.classList.toggle('lg:w-[300px]');
+    sidebar.classList.toggle('lg:w-0');
+
+    // sidebar open btn handling
+    humburger.classList.toggle('lg:hidden');
+
+    // fixed sidebar handling
+    fixedSidebar?.classList?.toggle('lg:w-[255px]');
+    fixedSidebar?.classList?.toggle('lg:w-0');
+
+    // fixed sidebar handling on small screen
+    fixedSidebar?.classList?.toggle('w-0');
+    fixedSidebar?.classList?.toggle('w-[260px]');
+}
 
 export default function Layout() {
     return (
-        <div>
-            <Outlet/>
+        <div className="flex transition-all duration-300 h-[100vh]">
+            <Sidebar onClickFn={toggleSidebar}/>
+            <div className="w-full">
+                <Header onClickFn={toggleSidebar}/>
+                <main className="bg-[#EEF3F8] p-7 min-h-[100vh]">
+                    <Outlet/>
+                </main>
+                <Footer/>
+            </div>
         </div>
     )
 }
--- resources/views/Dashboard/app.blade.php
+++ resources/views/Dashboard/app.blade.php
@@ -6,6 +6,7 @@
 
         <title>Laravel</title>
         @vite('resources/css/app.css')
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     </head>
     <body>
         <div id="root"></div>
