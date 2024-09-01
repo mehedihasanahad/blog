007607086052f5e2acfc9c794d4ea06f3c428ce6
Mehedi Hasan Ahad <mehedihasanahad07@gmail.com>
2024-09-01 22:59:41 +0600

    introduce useCallback and memo functionality

--- resources/js/admin/layout/Sidebar.jsx
+++ resources/js/admin/layout/Sidebar.jsx
@@ -1,17 +1,21 @@
-export default function Sidebar({ onClickFn }) {
+import { memo } from "react";
+
+function Sidebar({ onClickFn }) {
     return (
         <>
             <div className="fixed w-[0px] overflow-hidden top-0 h-[100vh] lg:static transition-all duration-300 lg:w-[300px] sidebar"></div>
-            <aside className="fixed top-0 left-0 bottom-0 z-[9999] shadow-lg w-[0px] lg:w-[255px] transition-all duration-300 overflow-hidden bg-white fixedSidebar">
+            <aside className="fixed top-0 left-0 bottom-0 z-[9999] shadow-lg w-[0px] lg:w-[258px] transition-all duration-300 overflow-hidden bg-white fixedSidebar">
                 <div className="h-[80px] px-4 py-2 flex justify-between items-center outline outline-[1px] outline-gray-200">
                     <div>
                         <img className="w-28" src="https://themesflat.co/html/remos/images/logo/logo.png" />
                     </div>
                     <div onClick={onClickFn} className="cursor-pointer">
-                        <i class="fa-regular fa-circle-left text-2xl transition-all duration-300 text-gray-400 hover:text-blue-400"></i>
+                        <i className="fa-regular fa-circle-left text-2xl transition-all duration-300 text-gray-400 hover:text-blue-400"></i>
                     </div>
                 </div>
             </aside>
         </>
     );
-  }
+}
+
+export default memo(Sidebar);
--- resources/js/admin/layout/index.jsx
+++ resources/js/admin/layout/index.jsx
@@ -2,34 +2,17 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer.jsx";
 import Header from './Header.jsx';
 import Sidebar from "./Sidebar.jsx";
-
-function toggleSidebar() {
-    const fixedSidebar = document.querySelector('.fixedSidebar');
-    const sidebar = document.querySelector('.sidebar');
-    const humburger = document.querySelector('.humburger');
-
-    // fake sidebar handling
-    sidebar.classList.toggle('lg:w-[300px]');
-    sidebar.classList.toggle('lg:w-0');
-
-    // sidebar open btn handling
-    humburger.classList.toggle('lg:hidden');
-
-    // fixed sidebar handling
-    fixedSidebar?.classList?.toggle('lg:w-[255px]');
-    fixedSidebar?.classList?.toggle('lg:w-0');
-
-    // fixed sidebar handling on small screen
-    fixedSidebar?.classList?.toggle('w-0');
-    fixedSidebar?.classList?.toggle('w-[260px]');
-}
+import { useCallback } from "react";
 
 export default function Layout() {
+    const toggleSidebarFn = useCallback(() => {
+        toggleSidebar();
+    }, []);
     return (
         <div className="flex transition-all duration-300 h-[100vh]">
-            <Sidebar onClickFn={toggleSidebar}/>
+            <Sidebar onClickFn={toggleSidebarFn}/>
             <div className="w-full">
-                <Header onClickFn={toggleSidebar}/>
+                <Header onClickFn={toggleSidebarFn}/>
                 <main className="bg-[#EEF3F8] p-7 min-h-[100vh]">
                     <Outlet/>
                 </main>
@@ -37,4 +20,29 @@ export default function Layout() {
             </div>
         </div>
     )
+}
+
+/**
+ * Toggle sidebar
+ * @return void
+ */
+function toggleSidebar() {
+    const fixedSidebar = document.querySelector('.fixedSidebar');
+    const sidebar = document.querySelector('.sidebar');
+    const hamburger = document.querySelector('.hamburger');
+
+    // fake sidebar handling
+    sidebar.classList.toggle('lg:w-[300px]');
+    sidebar.classList.toggle('lg:w-0');
+
+    // sidebar open btn handling
+    hamburger.classList.toggle('lg:hidden');
+
+    // fixed sidebar handling
+    fixedSidebar?.classList?.toggle('lg:w-[258px]');
+    fixedSidebar?.classList?.toggle('lg:w-0');
+
+    // fixed sidebar handling on small screen
+    fixedSidebar?.classList?.toggle('w-0');
+    fixedSidebar?.classList?.toggle('w-[260px]');
 }
